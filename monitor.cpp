c0592b4f1d363a25e3c1ba7332cf73fbad6635c4
PRAGADESH S K <88279902+pragadesh-sk@users.noreply.github.com>
2025-07-29 15:30:31 +0530

    Update monitor code approach

--- monitor.cpp
+++ monitor.cpp
@@ -6,33 +6,38 @@
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
+return vitalTemp(temperature)&&vitalPulseRate(pulseRate)&&vitalSpo2(spo2);
+}
+
+int vitalTemp(float temperature) {
   if (temperature > 102 || temperature < 95) {
     cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+    delay();
+      return 0;
     }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
+    return 1;
   }
+  int vitalPulseRate(float pulseRate) {
+  if (pulseRate < 60 || pulseRate > 100) {
+    cout << "Pulse Rate is out of range!\n";
+    delay();
+    return 0;
+    }
+    return 1;
+  }
+  int vitalSpo2(float spo2) {
+    if (spo2 < 90) {
+    cout << "Oxygen Saturation out of range!\n";
+    delay();
+    return 0;
+    }
   return 1;
+  }
+void delay() {
+for (int i = 0; i < 6; i++) {
+      cout << "\r* " << flush;
+      sleep_for(seconds(1));
+      cout << "\r *" << flush;
+      sleep_for(seconds(1));
+    }
 }
