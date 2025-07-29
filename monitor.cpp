232ff8d884bc969a47c59c1d0aa4ddce32fdcb8e
PRAGADESH S K <88279902+pragadesh-sk@users.noreply.github.com>
2025-07-29 15:33:51 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -6,34 +6,34 @@
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-return vitalTemp(temperature)&&vitalPulseRate(pulseRate)&&vitalSpo2(spo2);
+return vit_Temp(temperature)&&vit_PulseRate(pulseRate)&&vit_Spo2(spo2);
 }
 
-int vitalTemp(float temperature) {
+int vit_Temp(float temperature) {
   if (temperature > 102 || temperature < 95) {
     cout << "Temperature is critical!\n";
-    delay();
+    time();
       return 0;
     }
     return 1;
   }
-  int vitalPulseRate(float pulseRate) {
+  int vit_PulseRate(float pulseRate) {
   if (pulseRate < 60 || pulseRate > 100) {
     cout << "Pulse Rate is out of range!\n";
-    delay();
+    time();
     return 0;
     }
     return 1;
   }
-  int vitalSpo2(float spo2) {
+  int vit_Spo2(float spo2) {
     if (spo2 < 90) {
     cout << "Oxygen Saturation out of range!\n";
-    delay();
+    time();
     return 0;
     }
   return 1;
   }
-void delay() {
+void time() {
 for (int i = 0; i < 6; i++) {
       cout << "\r* " << flush;
       sleep_for(seconds(1));
